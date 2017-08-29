a7207a923e75858072694f7637ea7b9fa271c7a5
bakatrouble <bakatrouble@gmail.com>
2017-08-29 10:48:43 +0300

    Update tg_echo.py
    add sending stdin/file contents; parse modes

--- bin/tg_echo.py
+++ bin/tg_echo.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
+from sys import stdin
+import argparse
+
 import telebot
-from sys import argv
 
 TOKEN = 'token'
 CHAT_ID = 1
 
-bot = telebot.TeleBot(TOKEN)
-bot.send_message(CHAT_ID, argv[1])
+parser = argparse.ArgumentParser(description='Send notification to Telegram')
+parser.add_argument('message', metavar='MESSAGE', type=str, nargs='*',
+                    help='Message to send, might be split to multiple arguments')
+parser.add_argument('--infile', nargs='?', type=argparse.FileType('r'),
+                    default=stdin)
+parser.add_argument('--parse', choices=['html', 'markdown'])
+args = parser.parse_args()
 
+bot = telebot.TeleBot(TOKEN)
+
+if args.message:
+    message = ' '.join(args.message)
+else:
+    message = ''.join(args.infile.readlines())
+
+parse = args.parse or None
+
+bot.send_message(CHAT_ID, message, parse_mode=parse)
